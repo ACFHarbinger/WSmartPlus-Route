f4a394ec4488a7068c22b4dd39a25980b6a0069e
ACFPeacekeeper <afonso.fernandes100@gmail.com>
2026-01-16 15:59:40 +0000

    Fixed exports/imports

--- logic/src/pipeline/reinforcement_learning/meta/__init__.py
+++ logic/src/pipeline/reinforcement_learning/meta/__init__.py
@@ -1,3 +1,17 @@
 """
 Meta-Learning Module for Adaptive Weight Optimization.
 """
+
+from .contextual_bandits import WeightContextualBandit
+from .multi_objective import MORLWeightOptimizer
+from .temporal_difference_learning import CostWeightManager
+from .weight_optimizer import RewardWeightOptimizer
+from .weight_strategy import WeightAdjustmentStrategy
+
+__all__ = [
+    "WeightAdjustmentStrategy",
+    "CostWeightManager",
+    "MORLWeightOptimizer",
+    "RewardWeightOptimizer",
+    "WeightContextualBandit",
+]
