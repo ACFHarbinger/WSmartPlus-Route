72561c42188f2fa5db873adb017e60f2a880c9a1
ACFPeacekeeper <afonso.fernandes100@gmail.com>
2026-03-11 17:38:46 +0000

    Implemented the k-opt and k-opt-star operators

--- logic/src/policies/other/operators/inter_route/__init__.py
+++ logic/src/policies/other/operators/inter_route/__init__.py
@@ -6,13 +6,15 @@ This package contains operators that perform moves between different routes.
 
 from .cross_exchange import cross_exchange
 from .ejection_chain import ejection_chain
+from .k_opt_star import move_2opt_star, move_3opt_star, move_kopt_star
 from .lambda_interchange import lambda_interchange
 from .swap_star import move_swap_star
-from .two_opt_star import move_2opt_star
 
 __all__ = [
     "move_swap_star",
     "move_2opt_star",
+    "move_3opt_star",
+    "move_kopt_star",
     "cross_exchange",
     "lambda_interchange",
     "ejection_chain",
