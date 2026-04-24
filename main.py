252ef34bb82f5ac0f8e3a44e664d789c488f4fbd
ACFPeacekeeper <afonso.fernandes100@gmail.com>
2026-04-24 20:24:01 +0100

    Updated simulator.spec and parser dispatch

--- main.py
+++ main.py
@@ -41,8 +41,8 @@ try:
 except ImportError:
     pass
 
-from logic.controller.hydra_dispatch import hydra_entry_point
-from logic.controller.parser_dispatch import parser_entry_point
+from logic.hydra_dispatch import hydra_entry_point
+from logic.parser_dispatch import parser_entry_point
 from logic.src.cli import parse_params
 
 warnings.filterwarnings(
