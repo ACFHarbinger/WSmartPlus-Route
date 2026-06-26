3e2a8e77d14c52dc5c0e254b6b2d1dc016cf3c7a
ACFPeacekeeper <afonso.fernandes100@gmail.com>
2026-06-26 14:32:07 +0100

    Added crawler to get data directly from HTML/URL websites

---
+++ logic/src/data/web/__init__.py
@@ -0,0 +1,8 @@
+from logic.src.data.web.dashboard_crawler import (
+    extract_dataframe,
+    to_csv,
+    to_excel,
+    to_simulation_data,
+)
+
+__all__ = ["extract_dataframe", "to_csv", "to_excel", "to_simulation_data"]
